12e9c3f3da4e362f8e31311602f90efe543de2d2
JamesBremner <james@ravenspoint.com>
2022-09-04 09:55:43 -0400

    Plot

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@
 #include <chrono>
 #include "date.h"
 #include <wex.h>
+#include "plot2d.h"
 #include "cStarterGUI.h"
 
-class cJob {
-    public:
-    cJob( const std::string& line );
+class cJob
+{
+public:
+    cJob(const std::string &line);
     void DisplayText();
 
     std::string myName;
@@ -21,18 +23,18 @@ class cJob {
     std::chrono::duration<int> myDuration;
 };
 
-cJob::cJob( const std::string& line )
+cJob::cJob(const std::string &line)
 {
     myName = line.substr(2, 1);
     std::stringstream ss(line.substr(5, 8));
     ss >> date::parse("%H:%M:%S", myStart);
-    
-     std::stringstream ss2(line.substr(19, 8));
-     ss2 >> date::parse("%H:%M:%S", myStop);
+
+    std::stringstream ss2(line.substr(19, 8));
+    ss2 >> date::parse("%H:%M:%S", myStop);
 
     myDuration = myStop - myStart;
 
-     std::cout << "Duration secs " << myDuration.count() << "\n";
+    std::cout << "Duration secs " << myDuration.count() << "\n";
 }
 
 void cJob::DisplayText()
@@ -40,24 +42,90 @@ void cJob::DisplayText()
     std::cout << myStart.count() << " to " << myStop.count() << ", ";
 }
 
+struct sPlotData
+{
+    std::vector<std::vector<double>> trace;
+    int min;
+    int max;
+    double scale;
+    double timeStartHours;
+};
+
 class cSolution
 {
 public:
     void read();
 
-    std::vector< std::vector< cJob > > myJob;
+    std::vector<std::vector<cJob>> myJob;
 
-    void addJob( const cJob& job );
+    void addJob(const cJob &job);
 
     void DisplayText();
+
+    sPlotData plotData();
+
+private:
+    void range(int &min, int &max);
 };
+
+sPlotData cSolution::plotData()
+{
+    sPlotData ret;
+    range(ret.min, ret.max);
+    int delta = (ret.max - ret.min) / 100;
+    ret.timeStartHours = ret.min / ( 60. * 60. );
+    ret.scale = delta / ( 60. * 60. );
+    double onValue = 30;
+    for (auto &vjob : myJob)
+    {
+        std::vector<double> vd;
+
+        for (
+            int t = ret.min;
+            t < ret.max + delta;
+            t += delta)
+        {
+            double v = 0;
+            for (auto &job : vjob)
+            {
+                if (job.myStart.count() <= t && t <= job.myStop.count())
+                {
+                    v = onValue;
+                    break;
+                }
+            }
+
+            vd.push_back(v);
+        }
+        std::cout << "\n";
+        ret.trace.push_back(vd);
+        onValue -= 10;
+    }
+    return ret;
+}
+
+void cSolution::range(int &min, int &max)
+{
+    min = 24 * 60 * 60;
+    max = 0;
+    for (auto &vjob : myJob)
+    {
+        for (auto &job : vjob)
+        {
+            if (job.myStart.count() < min)
+                min = job.myStart.count();
+            if (job.myStop.count() > max)
+                max = job.myStop.count();
+        }
+    }
+}
 
 void cSolution::DisplayText()
 {
-    for( auto& vjob : myJob )
+    for (auto &vjob : myJob)
     {
         std::cout << vjob[0].myName << ": ";
-        for ( auto& job : vjob )
+        for (auto &job : vjob)
         {
             job.DisplayText();
         }
@@ -65,20 +133,20 @@ void cSolution::DisplayText()
     }
 }
 
- void cSolution::addJob( const cJob& job )
- {
-    for( auto& vjob : myJob )
+void cSolution::addJob(const cJob &job)
+{
+    for (auto &vjob : myJob)
     {
-        if( job.myName == vjob[0].myName )
-            {
-                vjob.push_back( job );
-                return;
-            }
+        if (job.myName == vjob[0].myName)
+        {
+            vjob.push_back(job);
+            return;
+        }
     }
-    std::vector< cJob > vjob;
-    vjob.push_back( job );
-    myJob.push_back( vjob );
- }
+    std::vector<cJob> vjob;
+    vjob.push_back(job);
+    myJob.push_back(vjob);
+}
 
 void cSolution::read()
 {
@@ -93,14 +161,53 @@ void cSolution::read()
     while (getline(f, line))
     {
         std::cout << line << "\n";
-        cJob job( line );
-        addJob( job );
+        cJob job(line);
+        addJob(job);
     }
 }
+class cGUI : public cStarterGUI
+{
+public:
+    cGUI()
+        : cStarterGUI(
+              "Starter",
+              {50, 50, 1000, 500}),
+          myPlot(wex::maker::make<wex::plot::plot>(fm))
+    {
+        myPlot.move({300, 30, 500, 600});
+
+        cSolution S;
+        S.read();
+        S.DisplayText();
+        auto pd = S.plotData();
+
+        myPlot.XValues(
+            pd.timeStartHours,
+            pd.scale );
+        wex::plot::trace &t1 = myPlot.AddStaticTrace();
+        wex::plot::trace &t2 = myPlot.AddStaticTrace();
+        wex::plot::trace &t3 = myPlot.AddStaticTrace();
+        t1.thick(6);
+        t2.thick(6);
+        t3.thick(6);
+        t1.color(0x0000FF);
+        t2.color(0xFF0000);
+
+        t1.set(pd.trace[0]);
+        t2.set(pd.trace[1]);
+        t3.set(pd.trace[2]);
+
+        show();
+        run();
+    }
+
+private:
+    wex::plot::plot &myPlot;
+};
 main()
 {
-    cSolution S;
-    S.read();
-    S.DisplayText();
+
+    cGUI theGUI;
+
     return 0;
 }
